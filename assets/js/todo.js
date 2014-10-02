7c8b321984548b9b5f548cee8f1dbdeecdff8cb2
Dustin Dong <fusiondreams@gmail.com>
2014-10-02 14:49:58 -0400

    fixed button

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('button').click(function(){
         var item = $('input[name=additem]').val();
-        $('ol').append('<li>'+ item +' <button class="delete">Done</button></li>');
+        $('ol').append('<li>'+ item +' <span class="delete">Done</span></li>');
     });
 
 $(document).on("click", ".delete", function(){
